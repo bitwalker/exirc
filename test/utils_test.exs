97f8824bfd51f7c58fc12e9fbaffdfc687ef9998
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-12-08 01:04:00 -0600

    Invoke doctest in utils tests

--- test/utils_test.exs
+++ test/utils_test.exs
@@ -7,6 +7,8 @@ defmodule ExIrc.UtilsTest do
   alias ExIrc.Client.IrcMessage, as: IrcMessage
   alias ExIrc.Client.ClientState, as: ClientState
 
+  doctest ExIrc.Utils
+
   test "Given a local date/time as a tuple, can retrieve get the CTCP formatted time" do
   	local_time = {{2013,12,6},{14,5,00}} # Mimics output of :calendar.local_time()
   	assert Utils.ctcp_time(local_time) == 'Fri Dec 06 14:05:00 2013'

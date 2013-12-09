ff5df43db73772e8df0bdcccdcc9bc2008cccad4
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-12-09 08:53:02 -0600

    Convert commands to binary strings

--- lib/exirc/commands.ex
+++ lib/exirc/commands.ex
@@ -9,45 +9,45 @@ defmodule Irc.Commands do
       # IRC Numeric Codes
       ####################
 
-      @rpl_welcome '001'
-      @rpl_yourhost '002'
-      @rpl_created '003'
-      @rpl_myinfo '004'
-      @rpl_isupport '005' # Defacto standard for server support
-      @rpl_bounce '010'   # Defacto replacement of '005' in RFC2812
-      @rpl_statsdline '250'
+      @rpl_welcome "001"
+      @rpl_yourhost "002"
+      @rpl_created "003"
+      @rpl_myinfo "004"
+      @rpl_isupport "005" # Defacto standard for server support
+      @rpl_bounce "010"   # Defacto replacement of "005" in RFC2812
+      @rpl_statsdline "250"
       #@doc """
       #":There are <integer> users and <integer> invisible on <integer> servers"
       #"""
-      @rpl_luserclient '251'
+      @rpl_luserclient "251"
       #@doc """
       # "<integer> :operator(s) online"
       #"""
-      @rpl_luserop '252'
+      @rpl_luserop "252"
       #@doc """
       #"<integer> :unknown connection(s)"
       #"""
-      @rpl_luserunknown '253'
+      @rpl_luserunknown "253"
       #@doc """
       #"<integer> :channels formed"
       #"""
-      @rpl_luserchannels '254'
+      @rpl_luserchannels "254"
       #@doc """
       #":I have <integer> clients and <integer> servers"
       #"""
-      @rpl_luserme '255'
+      @rpl_luserme "255"
       #@doc """
       #Local/Global user stats
       #"""
-      @rpl_localusers '265'
-      @rpl_globalusers '266'
+      @rpl_localusers "265"
+      @rpl_globalusers "266"
       #@doc """
       #When sending a TOPIC message to determine the channel topic, 
       #one of two replies is sent. If the topic is set, RPL_TOPIC is sent back else
       #RPL_NOTOPIC.
       #"""
-      @rpl_notopic '331'
-      @rpl_topic '332'
+      @rpl_notopic "331"
+      @rpl_topic "332"
       #@doc """
       #To reply to a NAMES message, a reply pair consisting
       #of RPL_NAMREPLY and RPL_ENDOFNAMES is sent by the
@@ -61,8 +61,8 @@ defmodule Irc.Commands do
 
       #Format: "<channel> :[[@|+]<nick> [[@|+]<nick> [...]]]"
       #"""
-      @rpl_namereply '353'
-      @rpl_endofnames '366'
+      @rpl_namereply "353"
+      @rpl_endofnames "366"
       #@doc """
       #When responding to the MOTD message and the MOTD file
       #is found, the file is displayed line by line, with
@@ -71,9 +71,9 @@ defmodule Irc.Commands do
       #by a RPL_MOTDSTART (before the RPL_MOTDs) and an
       #RPL_ENDOFMOTD (after).
       #"""
-      @rpl_motd '372'
-      @rpl_motdstart '375'
-      @rpl_endofmotd '376'
+      @rpl_motd "372"
+      @rpl_motdstart "375"
+      @rpl_endofmotd "376"
 
       ################
       # Error Codes
@@ -82,70 +82,70 @@ defmodule Irc.Commands do
       #@doc """
       #Used to indicate the nickname parameter supplied to a command is currently unused.
       #"""
-      @err_no_such_nick '401'
+      @err_no_such_nick "401"
       #@doc """
-      #Used to indicate the server name given currently doesn't exist.
+      #Used to indicate the server name given currently doesn"t exist.
       #"""
-      @err_no_such_server '402'
+      @err_no_such_server "402"
       #@doc """
       #Used to indicate the given channel name is invalid.
       #"""
-      @err_no_such_channel '403'
+      @err_no_such_channel "403"
       #@doc """
       #Sent to a user who is either (a) not on a channel which is mode +n or (b),
       #not a chanop (or mode +v) on a channel which has mode +m set, and is trying 
       #to send a PRIVMSG message to that channel.
       #"""
-      @err_cannot_send_to_chan '404'
+      @err_cannot_send_to_chan "404"
       #@doc """
       #Sent to a user when they have joined the maximum number of allowed channels 
       #and they try to join another channel.
       #"""
-      @err_too_many_channels '405'
+      @err_too_many_channels "405"
       #@doc """
       #Returned to a registered client to indicate that the command sent is unknown by the server.
       #"""
-      @err_unknown_command '421'
+      @err_unknown_command "421"
       #@doc """
-      #Returned when a nickname parameter expected for a command and isn't found.
+      #Returned when a nickname parameter expected for a command and isn"t found.
       #"""
-      @err_no_nickname_given '431'
+      @err_no_nickname_given "431"
       #@doc """
       #Returned after receiving a NICK message which contains characters which do not fall in the defined set.
       #"""
-      @err_erroneus_nickname '432'
+      @err_erroneus_nickname "432"
       #@doc """
       #Returned when a NICK message is processed that results in an attempt to 
       #change to a currently existing nickname.
       #"""
-      @err_nickname_in_use '433'
+      @err_nickname_in_use "433"
       #@doc """
       #Returned by a server to a client when it detects a nickname collision
       #(registered of a NICK that already exists by another server).
       #"""
-      @err_nick_collision '436'
+      @err_nick_collision "436"
       #@doc """
       #"""
-      @err_unavail_resource '437'
+      @err_unavail_resource "437"
       #@doc """
       #Returned by the server to indicate that the client must be registered before 
       #the server will allow it to be parsed in detail.
       #"""
-      @err_not_registered '451'
+      @err_not_registered "451"
       #"""
       # Returned by the server by numerous commands to indicate to the client that 
-      # it didn't supply enough parameters.
+      # it didn"t supply enough parameters.
       #"""
-      @err_need_more_params '461'
+      @err_need_more_params "461"
       #@doc """
       #Returned by the server to any link which tries to change part of the registered 
       #details (such as password or user details from second USER message).
       #"""
-      @err_already_registered '462'
+      @err_already_registered "462"
       #@doc """
       #Returned by the server to the client when the issued command is restricted
       #"""
-      @err_restricted '484'
+      @err_restricted "484"
 
       ###############
       # Code groups
@@ -160,8 +160,8 @@ defmodule Irc.Commands do
   end
 
   # Helpers
-  defmacro send!(socket, data) do
-    quote do: :gen_tcp.send(unquote(socket), unquote(data))
+  def send!(socket, data) do
+    :gen_tcp.send(socket, data)
   end
 
   def command!(cmd) when is_binary(cmd), do: command! String.to_char_list!(cmd)

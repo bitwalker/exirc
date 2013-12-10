5f6552890f1eee79193e9e8168610dfd75c6b285
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-12-09 19:59:38 -0600

    Add expm package definition

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "exirc", description: "An IRC client library written in Elixir",
+                 version: "0.1.0", keywords: ["irc", "client", "library", "elixir"], 
+                 maintainers: [[name: "Paul Schoenfelder", 
+                                email: "paulschoenfelder@gmail.com"]],
+                 repositories: [[github: "bitwalker/exirc"]])
+    

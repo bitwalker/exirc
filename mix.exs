c1262d8f47b9fd139ed14b9f4518917960e44535
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-12-20 14:23:33 -0600

    Version 0.2.0 - Built for Elixir 0.12.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExIrc.Mixfile do
 
   def project do
     [ app: :exirc,
-      version: "0.1.1",
+      version: "0.2.0",
       name: "ExIrc",
       source_url: "https://github.com/bitwalker/exirc",
       homepage_url: "http://bitwalker.github.io/exirc",

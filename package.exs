3f1c48b2a01057ca878f56c4ac79975e9f126644
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-02-19 18:05:15 -0600

    Version 0.2.1

--- package.exs
+++ package.exs
@@ -2,7 +2,7 @@ Expm.Package.new(
   name: "exirc", 
   description: "An IRC client library written in Elixir",
   homepage: "https://bitwalker.github.io/exirc",
-  version: "0.2.0",
+  version: "0.2.1",
   keywords: ["irc", "client", "library", "elixir"], 
   maintainers: [[name: "Paul Schoenfelder", email: "paulschoenfelder@gmail.com"]],
   repositories: [[github: "bitwalker/exirc"]]

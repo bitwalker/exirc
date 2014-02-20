033ed51d3133569eb62d07b7a97fc80a55311c49
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-02-19 21:42:52 -0600

    Version 0.3.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExIrc.Mixfile do
 
   def project do
     [ app: :exirc,
-      version: "0.3.0",
+      version: "0.3.1",
       name: "ExIrc",
       source_url: "https://github.com/bitwalker/exirc",
       homepage_url: "http://bitwalker.github.io/exirc",

901b332c8b5194344bf94d637bbfe183f46348c0
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-04-22 22:52:20 -0500

    Add hex.pm metadata

--- mix.exs
+++ mix.exs
@@ -4,10 +4,8 @@ defmodule ExIrc.Mixfile do
   def project do
     [ app: :exirc,
       version: "0.3.1",
-      name: "ExIrc",
-      source_url: "https://github.com/bitwalker/exirc",
-      homepage_url: "http://bitwalker.github.io/exirc",
-      deps: deps ]
+      description: "An IRC client library for Elixir.",
+      deps: [] ]
   end
 
   # Configuration for the OTP application
@@ -15,12 +13,12 @@ defmodule ExIrc.Mixfile do
     [mod: {ExIrc.App, []}]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat.git" }
-  defp deps do
-    []
+  defp package do
+    [ files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Paul Schoenfelder"],
+      licenses: ["MIT"],
+      links: [ { "GitHub", "https://github.com/bitwalker/exirc" },
+               { "Home Page", "http://bitwalker.org/exirc"} ] ]
   end
+
 end

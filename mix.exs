2ff34ee91d45e9390869efb07ac550f89b450291
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-05-02 00:37:09 -0500

    Can only support 0.13.1 and above

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExIrc.Mixfile do
   def project do
     [ app: :exirc,
       version: "0.4.0",
-      elixir: "~> 0.13.0-dev",
+      elixir: "~> 0.13.1",
       description: "An IRC client library for Elixir.",
       package: package,
       deps: [] ]

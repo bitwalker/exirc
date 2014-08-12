e494c8fec6642db276df68ff8d2853527f3203d6
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-08-12 13:24:00 -0500

    Fix bug in Utils.parse_from

--- lib/exirc/utils.ex
+++ lib/exirc/utils.ex
@@ -25,20 +25,23 @@ defmodule ExIrc.Utils do
     end
   end
 
+  @split_pattern ~r/(!|@|\.)/
   defp parse_from(from, msg) do
-    binary_from = IO.iodata_to_binary(from)
-    fully_qualified_regex = ~r/(?<nick>.*)!(?<user>.*)@(?<host>.*)/
-    missing_user_regex = ~r/(?<nick>.*)@(?<host>.*)/
-    host_only_regex = ~r/.+\..+/
-    cond do
-      captures = Regex.named_captures fully_qualified_regex, binary_from ->
-        %{msg | :nick => captures[:nick], :user => captures[:user], :host => captures[:host]}
-      captures = Regex.named_captures missing_user_regex, binary_from ->
-        %{msg | :nick => captures[:nick], :host => captures[:host]}
-      Regex.match? host_only_regex, binary_from ->
+    from_str = IO.iodata_to_binary(from)
+    splits   = Regex.scan(@split_pattern, from_str, return: :index) 
+               |> Enum.map(fn [{start, len},_] -> binary_part(from_str, start, len) end)
+    parts    = Regex.split(@split_pattern, from_str)
+    woven    = weave(splits, parts)
+    case woven do
+      [nick, "!", user, "@" | host] ->
+        %{msg | :nick => nick, :user => user, :host => Enum.join(host)}
+      [nick, "@" | host] ->
+        %{msg | :nick => nick, :host => Enum.join(host)}
+      [_, "." | _] ->
+        # from is probably a server name
         %{msg | :server => to_string(from)}
-      true ->
-        %{msg | :nick => to_string(from)}
+      [nick] ->
+        %{msg | :nick => nick}
     end
   end
 
@@ -171,5 +174,10 @@ defmodule ExIrc.Utils do
       _ -> charlist
     end
   end
+
+  defp weave(xs, ys), do: do_weave(xs, ys, [])
+  defp do_weave([], ys, result),           do: (ys ++ result) |> Enum.reverse
+  defp do_weave(xs, [], result),           do: (xs ++ result) |> Enum.reverse
+  defp do_weave([hx|xs], [hy|ys], result), do: do_weave(xs, ys, [hx, hy | result])
 
 end

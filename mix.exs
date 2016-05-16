51ed5e20c557cf5624e8ed07083705d3d278ca5c
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-05-16 09:17:14 -0500

    Version 0.11

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExIrc.Mixfile do
 
   def project do
     [ app: :exirc,
-      version: "0.10.0",
+      version: "0.11.0",
       elixir: "~> 1.0",
       description: "An IRC client library for Elixir.",
       package: package,

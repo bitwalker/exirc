847e2a095c721dd3b68192425b949050082ea7e5
Dom Rodriguez <shymega@shymega.org.uk>
2017-04-20 23:03:07 +0200

    Add test for cloaked user

--- test/utils_test.exs
+++ test/utils_test.exs
@@ -25,6 +25,19 @@ defmodule ExIrc.UtilsTest do
     assert expected == result
   end
 
+  test "Parse cloaked user" do
+    message = ':foo!foo@unaffiliated/foo PRIVMSG #bar Hiya.'
+    expected = %IrcMessage{
+      nick: "foo",
+      cmd: "PRIVMSG",
+      host: "unaffiliated/foo",
+      ctcp: false,
+      user: "foo",
+      args: ["#bar", "Hiya."]
+    }
+    result = Utils.parse(message)
+    assert expected == result
+  end
   test "Parse INVITE message" do
     message = ':pschoenf INVITE testuser #awesomechan'
     assert %IrcMessage{
